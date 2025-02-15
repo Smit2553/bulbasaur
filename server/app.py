3d1ac4b6b9a03ac7f8d04775dff5f0c7ce726f66
Nga Vu <vuthiquynhngahvn@gmail.com>
2025-02-15 14:05:09 -0700

    Add prediction route and load wildfire classification model

--- server/app.py
+++ server/app.py
@@ -1,15 +1,22 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import pickle
+import pandas as pd
+import base64
+import cv2
+import numpy as np
 
 app = Flask(__name__)
+
+# Load the machine learning model from a pickle file
+classification_model = pickle.load(open("wildfire_classification_model.pkl", "rb"))
+
 CORS(app)
 
 # Class that holds the following info:
 # Active Fires
 # Acres Burned
 # Containment
-
-
 class QuickStats:
     def __init__(self, active_fires, acres_burned, containment):
         self.active_fires = active_fires
@@ -35,6 +42,31 @@ def info_route():
     quick_stats = QuickStats(0, 0, 0)
     return jsonify(quick_stats.get_quick_stats())
 
+# Define a route for making predictions
+@app.route("/predict", methods=["POST"])
+def predict():
+    # Get JSON data from the request
+    json_ = request.get_json()
+
+    # Convert JSON data into an image
+    image = base64.b64decode(json_["image"])
+    image = cv2.resize(image,(32,32))
+    image = np.array(image)
+    image = image/255
+
+    # Use the loaded model to make predictions on the DataFrame
+    prediction = classification_model.predict(np.array([image]))
+
+    # Convert the prediction into a label
+    # Define label based on the probability
+    label = {
+        0: "No wildfire",
+        1: "Wildfire"
+    }
+    predicted_label = label[(prediction > 0.5).astype('int32')[0][0]]
+
+    # Return the predictions as a JSON response
+    return jsonify({"Prediction": predicted_label})
 
 if __name__ == '__main__':
     # Allow CORS
